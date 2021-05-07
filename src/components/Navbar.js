5acfc92ee2da0bfe36e81760eb8021d3bf06d746
CHANG Ern Rae <erchang.2017@smu.edu.sg>
2021-05-07 15:14:09 +0800

    conditional navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,20 @@ export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
+    const user = localStorage.getItem('user')
+    console.log("dhsnd", user);
+
+
     function logout() {
         console.log('Clicked')
         window.localStorage.clear();
         window.location.href = '/';
     }
 
-    return (
+    return ( 
         <>
+        {user != null ? 
+
             <Navbar color="dark" dark expand="md" className="px-2">
                 <Nav className="me-auto" navbar>
                         <NavItem>
@@ -37,6 +43,20 @@ export default function Header() {
                 </Collapse>
                 
               </Navbar>
-        </>
+
+          :
+          <Navbar color="dark" dark expand="md" className="px-2">
+          <NavbarToggler onClick={toggle}/>
+          <Collapse isOpen={isOpen} navbar>
+              <Nav className="ms-auto" navbar>
+                  <NavLink href='/'> Login</NavLink>
+              </Nav>
+
+          </Collapse>
+          
+        </Navbar>
+          }
+          </>
+        
     )
 }
