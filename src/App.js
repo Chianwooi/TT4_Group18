7ae235fc296074ce83134c9d6c7d1e1b6be13b0e
tengfone <phangtf@gmail.com>
2021-05-07 16:08:46 +0800

    tag history

--- src/App.js
+++ src/App.js
@@ -1,53 +1,73 @@
 import './App.css';
+
 import { Router, Route, Switch, BrowserRouter } from "react-router-dom";
+
 import Header from './components/Navbar'
+
 import Home from './Pages/Home'
+
 import Balance from './Pages/Balance'
+
 import Transfer from './Pages/Transfer'
+
 import History from './Pages/History'
+
 import Login from './Pages/Login'
+
 import { useState, useEffect } from 'react';
+
 import { Container } from 'reactstrap';
+
 import AuthenticatedRoute from './AuthenticatedRoute';
 
+
+
 function App() {
-  const [token, setToken] = useState();
 
-  const checkLocalStorage = () => {
-    let currentUser = localStorage.getItem('user')
-    setToken(currentUser)
-  }
-
-  useEffect(() => {
-    checkLocalStorage()
-    return () => {
-    }
-  }, [])
-
-  if(!token) {
-    // return <><Login setToken={setToken} /><History /></>
-    return <><History /></>
-  }
   return (
+
     <>
+
       <div className='App mr-auto'>
-        {/* <header>
+
+        <header>
+
           <Header/>
+
         </header>
+
         <Container className="fluid">
+
           <BrowserRouter forceRefresh={true}>
+
             <Switch>
+
               <Route exact path='/' component={Login}></Route>
+
               <AuthenticatedRoute path='/home' component={Home} />
+
               <AuthenticatedRoute path='/balance' component={Balance} />
-				      <AuthenticatedRoute path='/transfer' component={Transfer} />
+
+              <AuthenticatedRoute path='/transfer' component={Transfer} />
+
               <AuthenticatedRoute path='/history' component={History} />
+
             </Switch>
+
           </BrowserRouter>
-        </Container> */}
+
+        </Container>
+
+
+
       </div>
+
     </>
+
   );
+
 }
+
+
 
 export default App;
