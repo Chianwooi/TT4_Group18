db0fa18aab0cd28ceadebcbb9b5b86e4b08d1b53
tengfone <phangtf@gmail.com>
2021-05-07 16:36:01 +0800

    updates

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -1,49 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import MaterialTable from 'material-table'
-
 export default function History() {
-
     const [results, setResults] = useState([]);
     const [currentUser, setCurrentUser] = useState();
-
-    useEffect(() => {
-        setCurrentUser(JSON.parse(window.localStorage.getItem('user')))
-        viewHistory() // comment this out, for test only
-        return () => {
-        }
-    }, [])
-
-    const viewHistory = () => {
-        let accountKey = currentUser.accountKey
-        let custID = currentUser.custID
-        const requestOptions = {
-            method: 'POST',
-            headers: { "x-api-key": "rcqYXzQ9PY1rQtUNJB9X56JOvnQWnf27S09nX8Rh", },
-            body: JSON.stringify({
-                custID: custID,
-                accountKey: accountKey
-            })
-        };
-        fetch('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view', requestOptions)
-            .then(function (response) {
-                if (!response.ok) {
-                    alert("Error")
-                }
-                return response.json()
-            }).then(data => {
-                // SUCESS 
-                console.log(data)
-                setResults(results)
-            })
-    }
-
+    // useEffect(() => {
+    //     setCurrentUser(JSON.parse(window.localStorage.getItem('user')))
+    //     viewHistory() // comment this out, for test only
+    //     return () => {
+    //     }
+    // }, [])
+    // const viewHistory = () => {
+    //     let accountKey = currentUser.accountKey
+    //     let custID = currentUser.custID
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         headers: { "x-api-key": "rcqYXzQ9PY1rQtUNJB9X56JOvnQWnf27S09nX8Rh", },
+    //         body: JSON.stringify({
+    //             custID: custID,
+    //             accountKey: accountKey
+    //         })
+    //     };
+    //     fetch('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view', requestOptions)
+    //         .then(function (response) {
+    //             if (!response.ok) {
+    //                 alert("Error")
+    //             }
+    //             return response.json()
+    //         }).then(data => {
+    //             // SUCESS 
+    //             console.log(data)
+    //             setResults(results)
+    //         })
+    // }
     const data = [
-        { custID: 'CID1', payeeID: 'PID1', date: 'date', amount: 'amount', egift: 'No', message: 'message' },
-        { custID: 'CID2', payeeID: 'PID2', date: 'date', amount: 'amount', egift: 'No', message: 'message' },
-        { custID: 'CID3', payeeID: 'PID3', date: 'date', amount: 'amount', egift: 'No', message: 'message' },
-        { custID: 'CID4', payeeID: 'PID4', date: 'date', amount: 'amount', egift: 'No', message: 'message' },
+        { custID: 18, payeeID: 0, date: 1620367241, amount: 1, egift: 'false', message: "Hello World" },
+        { custID: 18, payeeID: 1, date: 1610957880, amount: 632.51, egift: 'false', message: "thank you" },
+        { custID: 18, payeeID: 2, date: 1610957880, amount: 750.62, egift: 'false', message: 'Lunch' },
+        { custID: 18, payeeID: 3, date: 1610957880, amount: 750.62, egift: 'false', message: '' },
+        { custID: 18, payeeID: 4, date: 1610957880, amount: 74.41, egift: 'false', message: '' },
+        { custID: 18, payeeID: 5, date: 1610957880, amount: 460.08, egift: 'false', message: 'DBS' },
     ]
-
     const columns = [
         {
             title: 'CustID', field: 'custID'
@@ -68,7 +64,6 @@ export default function History() {
             title: 'Message', field: 'message'
         }
     ]
-
     return (
         <div>
             <MaterialTable title='Transaction Details'
