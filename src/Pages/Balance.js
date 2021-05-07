9e9132ef0aafd403e176b6e1dc7fca3d98daf269
tengfone <phangtf@gmail.com>
2021-05-07 15:48:42 +0800

    fixed balance page

--- src/Pages/Balance.js
+++ src/Pages/Balance.js
@@ -4,7 +4,7 @@ import BalanceCard from '../components/BalanceCard';
 export default function Balance() {
 
     // const [results, setResults] = useState([]);
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState();
 
     let currentUser = JSON.parse(window.localStorage.getItem('user'));
 
@@ -18,9 +18,9 @@ export default function Balance() {
         // POST request using fetch inside useEffect React hook
         const requestOptions = {
             method: 'POST',
-            headers: { "x-api-key": "rcqYXzQ9PY1rQtUNJB9X56JOvnQWnf27S09nX8Rh",  },
-            body: JSON.stringify({ 
-                custID: currentUser.custID,	
+            headers: { "x-api-key": "rcqYXzQ9PY1rQtUNJB9X56JOvnQWnf27S09nX8Rh", },
+            body: JSON.stringify({
+                custID: currentUser.custID,
                 accountKey: currentUser.accountKey,
                 // userPass: "lIozkWgKHpyJvs0" 
             })
@@ -32,17 +32,24 @@ export default function Balance() {
                 setResults(data);
                 // console.log(results);
             });
-    
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+
+        // empty dependency array means this effect will only run once (like componentDidMount in classes)
     }, []);
-    
+
     return (
-        
+
         <div>
             <h1>testing balance Page</h1>
             <p>This is the users current balance right now</p>
-            <BalanceCard {...results}  />
-
+            {
+                results && <>
+                    {
+                        results.map((eachBalance,index)=>{
+                            return <BalanceCard {...eachBalance} ></BalanceCard>
+                        })
+                    }
+                </>
+            }
             {/* { results.map( (result, index) =>{
                 return(
                 <BalanceCard
--- src/components/BalanceCard.js
+++ src/components/BalanceCard.js
@@ -1,38 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
 const BalanceCard = (props) => {
-
-
-    useEffect( () =>{
+    useEffect(() => {
         console.log(props);
-    })
+    }, [])
 
-    
-    const [data, setData] = useState([]);
-
-    return (      
-        <div>
-            {/* {props.map((prop) => {
-                return(
-                    <div className="balance-card">  
-                            <p>Name: {prop.accountName}</p>
-                            <p>Account Number: {prop.accountNumber}</p>
-                            <p>Available Balance: {prop.availableBal} </p>
-                        </div> 
-                    </div>
-                )
-            })} */}
-        </div> 
-    );
-}
- 
-export default BalanceCard;
-
-
-{/* <div className="balance-card">  
-          <div className="user-details">
+    return (
+        <div className="balance-card">
             <p>Name: {props.accountName}</p>
             <p>Account Number: {props.accountNumber}</p>
             <p>Available Balance: {props.availableBal} </p>
-            </div> 
-     </div> */}
+        </div>
+    );
+}
+
+export default BalanceCard;
