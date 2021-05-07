b983012ce6df5c4ac59313306b4e3e5b42cae7c1
ChokHaoZe <haoze_chok@mymail.sutd.edu.sg>
2021-05-07 15:11:47 +0800

    history changes

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -1,28 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MaterialTable from 'material-table'
 
 export default function History() {
 
-    const data=[
-        {name: 'person1', balance: 'balance1'},
-        {name: 'person2', balance: 'balance2'},
-        {name: 'person3', balance: 'balance3'},
-        {name: 'person4', balance: 'balance4'}
+    const [results, setResults] = useState([]);
 
+    let currentUser = JSON.parse(window.localStorage.getItem('user'));
+    // console.log(currentUser.custID);
+
+    // useEffect(() => {
+    // const requestOptions = {
+    // method: 'POST',
+    // headers: { "x-api-key": "rcqYXzQ9PY1rQtUNJB9X56JOvnQWnf27S09nX8Rh", },
+    // body: JSON.stringify({ 
+    // custID: currentUser.custID, 
+    // accountKey: currentUser.accountKey,
+    // // userPass: "lIozkWgKHpyJvs0" 
+    // })
+    // };
+    // fetch('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view', requestOptions)
+    // .then(response => response.json())
+    // .then(data => {
+    // // console.log(data);
+    // setResults(data);
+    // console.log(results);
+    // });
+    // }, []);
+
+    const data=[
+        {custID: 'CID1', payeeID: 'PID1', date: 'date', amount: 'amount', egift: 'No', message: 'message'},
+        {custID: 'CID2', payeeID: 'PID2', date: 'date', amount: 'amount', egift: 'No', message: 'message'},
+        {custID: 'CID3', payeeID: 'PID3', date: 'date', amount: 'amount', egift: 'No', message: 'message'},
+        {custID: 'CID4', payeeID: 'PID4', date: 'date', amount: 'amount', egift: 'No', message: 'message'},
     ]
 
     const columns=[
         {
-            title:'Name', field: 'name'
+            title:'CustID', field: 'custID'
         },
         {
-            title:'Balance', field: 'balance'
+            title:'PayeeID', field: 'payeeID'
+        }
+        ,
+        {
+            title:'Date', field: 'date'
+        }
+        ,
+        {
+            title:'Amount', field: 'amount'
+        }
+        ,
+        {
+            title:'eGift', field: 'egift'
+        }
+        ,
+        {
+            title:'Message', field: 'message'
         }
     ]
 
     return (
         <div>
-            <MaterialTable title='Material Table'
+            <MaterialTable title='Transaction Details'
             data={data}
             columns={columns}
             />
